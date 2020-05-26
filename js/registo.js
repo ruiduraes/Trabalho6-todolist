294e4b7f0c50aebb41350686f2070f048123d8e7
ruiduraes <61848275+ruiduraes@users.noreply.github.com>
2020-05-26 13:32:02 +0100

    Update registo.js

--- js/registo.js
+++ js/registo.js
@@ -1,14 +1,11 @@
-
-
-function submitForm() {  
+function submitForm(){  
     if(validation) { // fails    
        alert("your message");  
        return false; 
     } 
- }
+}
 
-function Validar() {
-
+function Validar(){
     var nome = document.getElementById("txt_nome").value;
     var apelido = document.getElementById("txt_apelido").value;
     var email = document.getElementById("txt_email").value;
@@ -18,35 +15,21 @@ function Validar() {
     var expresion = /\w+@\w+\.+[a-z]{2,3}/; 
     //Expressao regular de um email, nome@dominio.url.
 
-    if(nome == "" || apelido == "")
-    {
+    if(nome == "" || apelido == ""){
         //Verificacao se os campos estao vazios
         alert("Complete todos os espaços.");
-    } 
-
-    else if(email=="")
-    {
+    } else if(email==""){
         alert("Introduza o seu email.")
-    } 
-
-    else if(!expresion.test(email))
-    {
+    } else if(!expresion.test(email)){
         //verifica a expressao do email
          alert("Endereço de email errado.");
-    } 
-
-    else if(password != password_2)
-    {
+    } else if(password != password_2){
          alert("As passwords não coincidem.");
-    } 
-    
-    else
-    {
+    } else{
         alert("Obrigado por se registar, agora pode fazer login.");
         location.href='inicio.html';
     }
 }
-
 
 function Limpar(){
     var limpeza = "";
@@ -59,35 +42,36 @@ function Limpar(){
     document.getElementById("txt_password_2").value = limpeza;
 }
 
-function Numeros(evt) {
+function Numeros(evt){
     //Funcao que apenas permite o input de numeros
     var theEvent = evt || window.event;
 
-    if (theEvent.type === 'paste') {
+    if (theEvent.type === 'paste'){
         key = event.clipboardData.getData('text/plain');
-    } else {
+    } else{
         var key = theEvent.keyCode || theEvent.which;
         key = String.fromCharCode(key);
     }
     var regex = /[0-9]|\./;
-    if( !regex.test(key) ) {
+
+    if( !regex.test(key)){
       theEvent.returnValue = false;
       if(theEvent.preventDefault) theEvent.preventDefault();
     }
 }
 
-function Letras(evt) {
+function Letras(evt){
     //Funcao que permite apenas o input de letras
     var theEvent = evt || window.event;
 
-    if (theEvent.type === 'paste') {
+    if (theEvent.type === 'paste'){
         key = event.clipboardData.getData('text/plain');
-    } else {
+    } else{
         var key = theEvent.keyCode || theEvent.which;
         key = String.fromCharCode(key);
     }
     var regex = /^[a-zA-Z Á á É é Í í Ó ó Ú ú Â â Ê ê Î î Ô ô Û û Ã ã Õ õ ç Ç]+$/;
-    if (!regex.test(key)) {
+    if (!regex.test(key)){
         theEvent.returnValue = false;
         if (theEvent.preventDefault) theEvent.preventDefault();
     }

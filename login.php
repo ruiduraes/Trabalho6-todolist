6c21a029d74c271dfa6343c9860669b3f2b97e96
ruiduraes <61848275+ruiduraes@users.noreply.github.com>
2020-05-26 15:23:21 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<form autocomplete="off" method="post" action="php/login.php">
+<form autocomplete="off" method="post" action="login.php">
 <head>
-    <link rel="stylesheet" type="text/css" href="css/login.css"> 
+    <link rel="stylesheet" type="text/css" href="login.css"> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">

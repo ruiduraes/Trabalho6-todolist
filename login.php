2d2a8af85624eaa346fdc48a756611754bbb4cd8
rnogueira19 <61839742+rnogueira19@users.noreply.github.com>
2020-05-26 11:41:02 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,25 +1,29 @@
-<?php
 
+<?php
+session_start(); // Starting Session
+$error = ''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+if (empty($_POST['txt_email']) || empty($_POST['txt_password'])) {
+  die(header("location: login.html"));
+}
+else{
+// Define $username and $password
 $Email = $_POST['txt_email'];
 $Pass = $_POST['txt_password'];
+// mysqli_connect() function opens a new connection to the MySQL server.
+$conn = mysqli_connect("localhost", "root", "", "to-do");
+// SQL query to fetch information of registerd users and finds user match.
+$query = "SELECT Email, password from registo where Email=? AND password=? LIMIT 1";
+// To protect MySQL injection for Security purpose
+$stmt = $conn->prepare($query);
+$stmt->bind_param($Email, $Pass);
+$stmt->execute();
+$stmt->bind_result($Email, $Pass);
+$stmt->store_result();
+if($stmt->fetch()) //fetching the contents of the row {
+$_SESSION['txt_email'] = $Email; // Initializing Session
+header("location: inicio.html"); // Redirecting To Profile Page
+}
 
-    $host = "localhost";
-    $dbusername = "root";
-    $dbpassword = "";
-    $dbname = "to-do";
-    // Criar conexao
-    $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
- 
-    
-    $result = mysqli_query("SELECT Email, Password FROM registo WHERE txt_email = '$Email' and txt_password = '$Pass'") 
-    or die("Falhou a conexão");
-    $row = mysqli_fetch_array($result);
-    if ($row['txt_email'] == $Email && $row['txt_password'] == $Pass) {  
-        echo "Bem vindo".$row['txt_email'];
-    }
-    else{
-        echo "Não se encontra registado.";
-    }
-
-
+}
 ?>

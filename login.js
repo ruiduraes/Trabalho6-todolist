0a66e837148d297046e61b0a3a65bab57df4fe13
rnogueira19 <61839742+rnogueira19@users.noreply.github.com>
2020-05-26 14:46:26 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,27 @@
+function Validar() {
+
+    var email = document.getElementById("login_email").value;
+    var password = document.getElementById("login_password").value;
+
+    var expresion = /\w+@\w+\.+[a-z]{2,3}/; 
+    //Expressao regular de um email, nome@dominio.url.
+
+
+    if(email=="")
+    {
+        errorbox("Introduza o seu email.")
+    }
+    
+    else if(!expresion.test(email))
+    {
+        //verifica a expressao do email
+        errorbox("Endereço de email errado.");
+    } 
+
+    else if(password=="")
+    {
+        errorbox("Introduza a password")
+    }
+
+    
+}

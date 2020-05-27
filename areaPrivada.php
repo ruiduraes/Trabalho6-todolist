3007671ee2ea1f324d5e1b08a4d34cf10b6a6ef4
rnogueira19 <61839742+rnogueira19@users.noreply.github.com>
2020-05-27 21:52:03 +0100

    Create areaPrivada.php

---
+++ areaPrivada.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="areaPrivada.css"> <!--Juncao com a pagina css-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To-Do</title>
+</head>
+<body>
+    <div class="topnav">
+      <div>
+        <a id="btn_home" onclick="location.href='inicio.html'">Home</a>
+        <a id="area_privada" onclick="location.href='areaPrivada.html'">Perfil</a>
+        <a type="file" id="imagem_perfil" ></a>
+      </div>
+        <form id="forma" autocomplete="off"><!--Cria a forma-->
+          
+          bll
+        </form>
+        
+    </div>
+    <script src="inicio.js">
+    </script>
+  </body>
+  </html>

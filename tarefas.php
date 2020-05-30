be5cd93ee7efba6132161177442c9a74caf93ecd
ruiduraes <61848275+ruiduraes@users.noreply.github.com>
2020-05-30 16:29:34 +0100

    Update tarefas.php

--- tarefas.php
+++ tarefas.php
@@ -1,33 +1,65 @@
+<?php
+session_start();
+if($_SESSION['id_user'] == ''){
+  header("location: index.html");
+}
+$id_user = $_SESSION['id_user'];
+
+$username = "root"; 
+$password = ""; 
+$database = "to-do"; 
+$mysqli = new mysqli("localhost", $username, $password, $database); 
+$query = "SELECT * FROM registo WHERE id = $id_user " ;
+
+//print_r($query);
+ 
+if ($result = $mysqli->query($query)) {
+    while ($row = $result->fetch_assoc()) {
+        $nome = $row['Nome'];
+        $sobrenome = $row["Sobrenome"];
+        //echo $Email;
+        //echo '<br>';
+        //echo $row['id'];
+        //echo '<br>';
+        //echo $row['Nome'];       
+    }
+    $result->free();
+} 
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" type="text/css" href="tarefas.css"> <!--Juncao com a pagina css-->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>To-Do</title>
+  <link rel="stylesheet" type="text/css" href="tarefas.css"> <!--Juncao com a pagina css-->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>To-Do</title>
 </head>
 
 <body>
 
   <div class="topnav">
-      <a type="button" class="active">Tarefas</a>
-      <a type="button" id="btn_perfil" onClick= "location.href='perfil.php'">Perfil</a>
-      <a type="button" id="btn_logout" onClick= "location.href='index.html'">Logout</a>
-    </div>
+    <a type="button" class="active">Tarefas</a>
+    <a type="button" id="btn_perfil" onClick= "location.href='perfil.php'">Perfil</a>
+    <a type="button" id="btn_logout" onClick= "location.href='index.html'">Logout</a>
+  </div>
 
-    <form class="form" id="forma" autocomplete="off"><!--Cria a forma-->       
+  <div id="name">
+    <p> Bem vindo, <?=$nome?> <?=$sobrenome?></p>
+  </div>
+
+  <form class="form" id="forma" autocomplete="off"><!--Cria a forma-->       
     <h1>Lista To-Do</h1>
     <div id="myDIV" class="header">
       <input type="text" id="myInput" placeholder="Adicione aqui a sua tarefa...">
       <span onclick="newElement()" type="submit" class="addBtn">Adicionar</span>
     </div>
-    <ul id="myUL"><!--Onde as tarefas serão guardadas-->
-    </ul>
+    <ul id="myUL"></ul><!--Onde as tarefas serão guardadas-->
   </form>
         
-    <script src="inicio.js">
-    </script>
+  <script src="inicio.js"></script>
 
-  </body>
-  </html>
+</body>
+</html>

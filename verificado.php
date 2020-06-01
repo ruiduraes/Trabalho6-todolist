fcdc9a8a9ebd7d98041ca57d70692e53f8a74a73
rnogueira19 <61839742+rnogueira19@users.noreply.github.com>
2020-06-01 01:11:59 +0100

    Update verificado.php

--- verificado.php
+++ verificado.php
@@ -15,7 +15,7 @@
     <div class="form" id="forma" autocomplete="off" >
       <h1>Confirmação de Email</h1>
       <input type="text" id="login_email" name="txt_email" placeholder="Endereço de E-mail">
-       <input type="submit" name="submit" id="btn_aceitar"  value="Login" onclick="validar()"> 
+       <input type="submit" name="submit" id="btn_aceitar"  value="Feito" onclick="validar()"> 
     </div>
 
    <script src="login.js" type="text/javascript"></script> 
@@ -25,19 +25,28 @@
 </html>
 <?php
 // Starting Session
-session_start(); 
+session_start();
+include "connection.php";
 
-$username = "root"; 
-$password = ""; 
-$database = "to-do"; 
-$mysqli = new mysqli("localhost", $username, $password, $database); 
-$query = "UPDATE registo SET verificado = True WHERE Email = $; " ;
+if(!empty($_POST)) {
+  $result = mysqli_query($conn,"SELECT Email, verificado FROM registo Where Email='" .$_POST["txt_email"] . "'");
+
+  $count = mysqli_num_rows($result);
+  $user = $result->fetch_assoc();
+  $Email = $_POST["txt_email"];
+  if($count==0) {
+    echo  "<script>alert('Email Inválido');</script>";
+  }else{
+      mysqli_query($conn,"UPDATE registo SET verificado = TRUE WHERE Email = '$Email' ");
+      echo  "<script>alert('Email verificado com sucesso');</script>";
+      header("location: login.php");
+      
+  }
+}
 
 
 
 
-        //var_dump($user['Email']);
-        header("location: login.php");
 
 
 ?>

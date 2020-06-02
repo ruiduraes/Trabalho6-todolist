96d241b3d9addaf44aa2d7642037774c648cb538
rnogueira19 <61839742+rnogueira19@users.noreply.github.com>
2020-06-02 21:13:24 +0100

    Update selecionar.php

--- selecionar.php
+++ selecionar.php
@@ -8,13 +8,19 @@ $mysqli = new mysqli("localhost", $username, $password, $database);
 
 $id_tarefa = $_GET['id'];
 
-mysqli_query($conn,"SELECT * From tarefa WHERE id_tarefa = $id_tarefa ");
+$query = "SELECT * From tarefa WHERE id_tarefa = $id_tarefa";
 
 $result = $mysqli->query($query);
 
-//mysqli_query($conn,"UPDATE registo SET selecionado = 1 WHERE id = $id_user ");
+$row = $result->fetch_assoc();
+$selecionado = $row['selecionado'];
 
-//header("location: tarefas.php");
+if ($selecionado == 0 || $selecionado == NULL){
+    mysqli_query($conn,"UPDATE tarefa SET selecionado = 1 WHERE id_tarefa = $id_tarefa ");
+}
+else{
+    mysqli_query($conn,"UPDATE tarefa SET selecionado = 0 WHERE id_tarefa = $id_tarefa ");
+}
 
-print_r($result)
+header("location: tarefas.php");
 ?>

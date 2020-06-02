7257287c02163f7b90288d5b8bced767f6a57c7d
rnogueira19 <61839742+rnogueira19@users.noreply.github.com>
2020-06-02 22:37:40 +0100

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -21,26 +21,9 @@ function closeForm() {
 //}
 
 
-// Cria um botão "fechar" e anexa-o a cada item da lista
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
 
 
 
-// Adiciona um visto na tarefa
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
 
 // Cria uma nova lista quando é clicado o botão adicionar
 function newElement() {
